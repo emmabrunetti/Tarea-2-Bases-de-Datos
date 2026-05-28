4f0193908492e875e9aff76c290e5bb3f9d8dba5
Emma Brunetti <emma.brunetti@uc.cl>
2026-05-28 16:39:20 -0400

    index

--- indexes.sql
+++ indexes.sql
@@ -11,11 +11,8 @@ CREATE INDEX idx_reviews_book_date ON reviews(book_id, review_date DESC);
 -- Índice 3: Ataca Q8 
 CREATE INDEX idx_users_lower_email ON users(LOWER(email));
 
-
-
--- Indices creados origialmente para atacar Q10 pero empeoró el tiempo total xd
+-- Indices creados origialmente para atacar Q10 pero empeoró el tiempo total
 -- reviews por review_date, book_id, user_id
---CREATE INDEX idx_reviews_date_user_book ON reviews(review_date, book_id, user_id);
-
+-- CREATE INDEX idx_reviews_date_user_book ON reviews(review_date, book_id, user_id);
 -- order_items por book_id
---CREATE INDEX idx_order_items_book_order ON order_items(book_id, order_id);
+-- CREATE INDEX idx_order_items_book_order ON order_items(book_id, order_id);
